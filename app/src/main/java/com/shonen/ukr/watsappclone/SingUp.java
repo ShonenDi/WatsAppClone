77fdb505d71de0da8855a080cd5b708e5cdf3eb7
Dima Usatyuk <Shonen1988@gmail.com>
2020-05-14 15:33:08 +0300

    first commit

---
+++ app/src/main/java/com/shonen/ukr/watsappclone/SingUp.java
@@ -0,0 +1,14 @@
+package com.shonen.ukr.watsappclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sing_up);
+    }
+}
